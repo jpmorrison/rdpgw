b6ab5461ae03a4d5d9229cf2d6b708729cd722a7
John Paul Morrison <jmorrison@bogomips.com>
2021-01-30 19:38:06 -0800

    update

--- rdweb/download.sh
+++ rdweb/download.sh
@@ -10,16 +10,22 @@ file_dir=rdwebclientmanagement.1.0.3
 file_name=$file_dir.nupkg
 
 
-echo wget -O $file_name  $file_url
+wget -O $file_name  $file_url
 
+if ! md5sum -c ./md5
+then
+	echo problem with download  $file_name
+	exit 1
+fi
+
+rm -rf $file_dir
 unzip $file_name -d $file_dir
 
-md5sum -c ./md5
 
+#grep "PackageCatalogUrl = 'https" $file_dir/RDWebClientManagement.psm1
+#PackageCatalogUrl='https://go.microsoft.com/fwlink/?linkid=2005418'
+eval `awk '/PackageCatalogUrl = .https/ {print "PackageCatalogUrl="$3 }'  $file_dir/RDWebClientManagement.psm1`
 
-grep "PackageCatalogUrl = 'https" $file_dir/RDWebClientManagement.psm1
-
-PackageCatalogUrl='https://go.microsoft.com/fwlink/?linkid=2005418'
 
 wget -O - $PackageCatalogUrl | jq .packages > packages.json
 #  "url": "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4LTEc"
@@ -28,10 +34,18 @@ package_ver=`jq -r '.[].version' packages.json`
 package_id=`jq -r '.[].packageId' packages.json`
 
 package_file=$package_id.$package_ver.zip
+package_dir=$package_id.$package_ver
 
-wget -O $package_file  $package_url
+wget -O $package_file  $package_url 
 
-unzip $package_file
+rm -rf $package_dir 
+if ! unzip $package_file -d $package_dir
+then 
+	echo problem with $package_file 
+	exit 1
+else
+	echo $package_file successfully extracted
+fi
 
 
 
